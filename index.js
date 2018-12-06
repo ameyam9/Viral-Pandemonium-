83ca461d3398e1a51a65f058164dc48eb677c6c5
Onariaginosa Igbinedion <Ona.Igbinedion@gmail.com>
2018-12-06 15:16:21 -0800

    fixed Character constructer and wall corners don't have holes

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-let backgroundImage, playerSprite, enemySprite;
+let backgroundImage, playerSprite, enemySprite, player, enemies, scarecrow;
 
 function preload(){
-backgroundImage = loadImage("vein.png");
-playerSprite = loadImage("virus.png");
-enemySprite = loadImage("whiteBloodCell.png");
+  backgroundImage = loadImage("vein.png");
+  playerSprite = loadImage("virus.png");
+  enemySprite = loadImage("whiteBloodCell.png");
+  player = new Character(30, 30, playerSprite, 0.05);
+  enemies = [
+    new Character(300, 0, enemySprite, 0.01),
+    new Character(300, 300, enemySprite, 0.03),
+    new Character(0, 300, enemySprite, 0.003),
+    new Character(20, 400, enemySprite, 0.02),
+  ];
 }
 
 
@@ -13,8 +20,8 @@ class Character {
   constructor(x, y, sprite, speed) {
     Object.assign(this, { x, y, sprite, speed });
   }
-  drawE() {
-    image(enemySprite,this.x,this.y);
+  draw() {
+    image(this.sprite,this.x,this.y);
   }
   drawP() {
     image(playerSprite,this.x,this.y);
@@ -24,14 +31,6 @@ class Character {
     this.y += (target.y - this.y) * this.speed;
   }
 }
-const player = new Character(30, 30, playerSprite, 0.05);
-const enemies = [
-  new Character(300, 0, enemySprite, 0.01),
-  new Character(300, 300, enemySprite, 0.03),
-  new Character(0, 300, enemySprite, 0.003),
-  new Character(20, 400, enemySprite, 0.02),
-];
-let scarecrow;
 
 function setup()  {
   createCanvas(800, 600);
@@ -40,13 +39,13 @@ function setup()  {
 
 function draw() {
   background(backgroundImage);
-  player.drawP();
+  player.draw();
   image(playerSprite,player.x,player.y);
-  enemies.forEach(enemy => enemy.drawE());
+  enemies.forEach(enemy => enemy.draw());
   player.move({x: mouseX, y: mouseY});
   enemies.forEach(enemy => enemy.move(scarecrow || player));
   if (scarecrow) {
-    scarecrow.drawP();
+    scarecrow.draw();
     scarecrow.ttl--;
     if (scarecrow.ttl < 0) {
       scarecrow = undefined;
@@ -56,13 +55,15 @@ function draw() {
 }
 
 function adjust() {
-  const characters = [player, ...enemies];
-  for (let i = 0; i < characters.length; i++) {
-    for (let j = i+1; j < characters.length; j++) {
-      pushOff(characters[i], characters[j]);
-    }
-    avoidWalls(characters[i]);
-  }
+ const characters = [player, ...enemies];
+ for (let i = 0; i < characters.length; i++) {
+   for (let j = i+1; j < characters.length; j++) {
+     pushOff(characters[i], characters[j]);
+     for (let k= 0; k<characters.length; k++){
+       avoidWalls(characters[k]);
+     }
+   }
+ }
 }
 
 function pushOff(c1, c2) {
